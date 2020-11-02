16613fd88c1265b8f03b0a536fb10630c9059b3e
Tom Krause <tom.krause@igd-r.fraunhofer.de>
2020-11-02 21:48:34 +0100

    improve ci

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    "env": {
+        "browser": false,
+        "node": true,
+        "es2021": true
+    },
+    "globals": {
+        "require": true,
+    },
+    "extends": "eslint:recommended",
+    "parserOptions": {
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "windows"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
