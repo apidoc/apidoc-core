519e04a9b4d0a95e8bf5fdda38be22d7031f9e27
Tom Krause <tom.krause@igd-r.fraunhofer.de>
2020-11-02 21:53:52 +0100

    remove line ending check

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,10 +17,6 @@ module.exports = {
             "error",
             4
         ],
-        "linebreak-style": [
-            "error",
-            "windows"
-        ],
         "quotes": [
             "error",
             "single"
